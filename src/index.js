d96d4d334e94aeb7fa465e276c4be3ac3c627c61
Evan <eroberts1998@icloud.com>
2023-04-16 19:58:13 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const toyCollection = document.querySelector("#toy-collection");
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -12,4 +13,79 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+
+
+// GET request to fetch all the toy objects, with the response data, make a <div class="card"> for each toy and add it to the toy-collection div
+
+ fetch("http://localhost:3000/toys", {
+  headers: {
+    "Content-Type": "application/json"
+  }
+ })
+ .then(response => response.json())
+ .then(data => { 
+
+  data.forEach(toy => { 
+    console.log(toy);
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <div class="card-body">
+      <h2 class="card-title">${toy.name}</h5>
+      <img class="toy-avatar" src="${toy.image}" alt="${toy.name}" >
+      <p>${toy.likes}</p>
+      <button type="button"  class="like-btn" id="${toy.id}">Like ❤️</button>
+      `
+    toyCollection.appendChild(card);
+
+    // Add event listener for the like button
+    card.querySelector(".like-btn").addEventListener("click", (e) => { 
+      e.preventDefault();
+      const id = e.target.id;
+      const card = e.target.parentElement;
+      const like = parseInt(card.querySelector('p').innerText) + 1;
+
+      fetch(`http://localhost:3000/toys/${id}`, { 
+        method: "PATCH",
+        headers: { 
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({ 
+          "likes": like
+        })
+      })
+        .then(response => response.json())
+        .then(data => { 
+          card.querySelector('p').innerText = `${like}`;
+        })
+    })
+  })
+ })
+
+ // When a user submits the toy form, two things should happen: POST request to create a new toy, with the response data, 
+ // toy should be added to the toy-collection WITHOUT reloading the page
+  
+document.querySelector('form').addEventListener('submit', (e) => { 
+  console.log('form submitted');
+  e.preventDefault();
+  const toyName = document.querySelector('#input-text').value;
+  const toyImage = document.querySelector('#input-image').value;
+  
+  fetch("http://localhost:3000/toys", { 
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+  },
+    body: JSON.stringify({ 
+      "name": toyName,
+      "image": toyImage,
+      "likes": 0
+    })
+
+})
+
+})
+
+})
